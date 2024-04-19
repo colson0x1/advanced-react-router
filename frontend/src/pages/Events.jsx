2931717625f45c8fbfe3c836e9c65536fcd8afd5
Colson <colsoncalifornication@gmail.com>
2024-04-19 12:54:20 +0545

    output dummy events: output list of dummy events to the events page + each list item should include a link to the respective event detail page

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,5 +1,32 @@
+import { Link } from 'react-router-dom';
+
+const DUMMY_EVENTS = [
+  {
+    id: 'e1',
+    title: 'Some event',
+  },
+  {
+    id: 'e2',
+    title: 'Another event',
+  },
+];
+
 function EventsPage() {
-  return <h1>EventsPage</h1>;
+  return (
+    <>
+      <h1>EventsPage</h1>
+      <ul>
+        {/* We can either write absolute path: to={`/events/${event.id}`}
+        Or we convert it to relative path: to={event.id} 
+        This relative path will be appended after the currently active routes path */}
+        {DUMMY_EVENTS.map((event) => (
+          <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default EventsPage;
