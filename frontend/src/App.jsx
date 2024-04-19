624febb35cc0ffb597ee7c27641ac7213249bc54
Colson <colsoncalifornication@gmail.com>
2024-04-19 11:57:22 +0545

    add routing and route definitions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,23 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import EditEventPage from './pages/EditEvent';
+import EventDetailPage from './pages/EventDetail';
+import EventsPage from './pages/Events';
+import HomePage from './pages/Home';
+import NewEventPage from './pages/NewEvent';
+
+// RouterProvider is needed to apply and activate our route definitions
+// createBrowserRouter fn is required to create these route definitions
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/events', element: <EventsPage /> },
+  { path: '/events/:eventId', element: <EventDetailPage /> },
+  { path: '/events/new', element: <NewEventPage /> },
+  { path: '/events/:eventId/edit', element: <EditEventPage /> },
+]);
+
 function App() {
-  return <div>Advanced React Router</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
